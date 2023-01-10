6fb7288c06ae3fc630a1c4169e19cb0250dca651
igorraknorwid <igorrakfed@gmail.com>
2023-01-10 09:37:48 +0100

    index.astro responsetive

--- src/components/OrderButton.tsx
+++ src/components/OrderButton.tsx
@@ -28,12 +28,12 @@ export default function OrderButton(props: {
   return (
     <div className='basis-full flex items-center justify-between  bg-white text-2xl font-bold  py-2 rounded-xl'>
       <button
-        className='text-3xl rounded-md hover:bg-slate-200 transition duration-300  py-5 px-5 ml-4'
+        className='text-3xl rounded-md hover:bg-slate-200 transition duration-300  py-3 lg:py-5 px-5 ml-4'
         onClick={decrease}
       >
         -
       </button>
-      <div className='flex gap-x-5'>
+      <div className='flex gap-x-5 text-lg lg:text-xl'>
         <p>{state}</p>
         <p>{setWordInRightWay(props.title, state)}</p>
       </div>
--- src/components/OrderLink.tsx
+++ src/components/OrderLink.tsx
@@ -7,10 +7,10 @@ interface IOrderLink {
 
 export default function OrderLink(props: IOrderLink) {
   return (
-    <div className='basis-full flex justify-center i py-7 bg-[#2457c6] hover:bg-[#2457c6b9] rounded-xl'>
+    <div className='basis-full flex justify-center i py-5 lg:py-7 bg-[#2457c6] hover:bg-[#2457c6b9] rounded-xl'>
       <a
         href={`/order?rooms=${props.parentState.room}&bedrooms=${props.parentState.bedroom}`}
-        className=' transition duration-300  text-white text-2xl font-bold  '
+        className=' transition duration-300  text-white text-lg lg:text-xl font-bold  '
       >
         Policz koszty<span className='ml-16 text-2xl'>&#8594;</span>
       </a>
