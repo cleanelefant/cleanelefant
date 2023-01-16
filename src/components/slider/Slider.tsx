88348115a55e785eeb13d8e53e414d8d55e4e0b0
igorraknorwid <igorrakfed@gmail.com>
2023-01-16 09:45:52 +0100

    slider flow implements

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import bg0 from "../../images/1.png";
+import bg0 from "../../images/bedroom .webp";
 import bg1 from "../../images/2.png";
 import bg2 from "../../images/3.png";
 import bg3 from "../../images/4.png";
@@ -8,18 +8,25 @@ export default function Slider() {
   const [state, setState] = React.useState(0);
 
   const images = [
-    { src: bg0, title: "SPRZĄTANIE POKOJU" },
-    { src: bg1, title: "W PRZEDPOKOJU" },
-    { src: bg2, title: "KUCHNIA" },
-    { src: bg3, title: "ŁAZIENKA" },
+    {
+      id: 0,
+      src: "https://res.cloudinary.com/zielona-g-ra/image/upload/v1673858121/clean/bedroom_wczmwd.webp",
+      title: "SPRZĄTANIE POKOJU",
+    },
+    { id: 1, src: bg1, title: "W PRZEDPOKOJU" },
+    { id: 2, src: bg2, title: "KUCHNIA" },
+    { id: 3, src: bg3, title: "ŁAZIENKA" },
   ];
+
+  const image = images?.find((image) => image.id === state);
 
   return (
     <div>
-      <div className="flex gap-x-5 justify-center items-center">
+      <div className='flex gap-x-5 justify-center items-center'>
         {images.map((image, index) => {
           return (
-            <button className="inline-block px-6 py-2.5 bg-white hover:text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            <button
+              className='inline-block px-6 py-2.5 bg-white hover:text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
               onClick={() => {
                 setState(index);
               }}
@@ -29,10 +36,10 @@ export default function Slider() {
           );
         })}
       </div>
-      <div className="max-w-[1000px] h-[580px] w-full m-auto relative group">
+      <div className='max-w-[74vw] h-[800px] w-full m-auto relative group'>
         <div
-          className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-          style={{ backgroundImage: `url(${images[state].src})` }}
+          className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+          style={{ backgroundImage: `url(${image.src})` }}
         ></div>
       </div>
     </div>
