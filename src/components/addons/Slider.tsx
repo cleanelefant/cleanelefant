375cfbb0c222afedda236fca4c9e23e81dcfbd01
igorraknorwid <igorrakfed@gmail.com>
2023-01-20 14:52:54 +0100

    motion

---
+++ src/components/addons/Slider.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Slider() {
+  return (
+    <div>
+      <motion.h2 animate={{ x: 300 }}>Hello</motion.h2>
+    </div>
+  );
+}
