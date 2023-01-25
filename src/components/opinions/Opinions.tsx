90bbfd3bd61b931903be3710ea1fcf25f1b339b6
Igor Rak <igorrakfed@gmail.com>
2023-01-25 16:47:37 +0100

    perfomance test 10

--- src/components/opinions/Opinions.tsx
+++ src/components/opinions/Opinions.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const opinions =[
-    {id:1, }
+    {id:1,user:"Dan",text:"jestem zadowolona ze sprzatania. punktualność, szybkość pracy, ładny zapach detergentów. wykonawca uprzejmy. dziękuję.",stars:5},
+    {id:1,user:"Dan",text:"jestem zadowolona ze sprzatania. punktualność, szybkość pracy, ładny zapach detergentów. wykonawca uprzejmy. dziękuję.",stars:5},
+    {id:1,user:"Dan",text:"jestem zadowolona ze sprzatania. punktualność, szybkość pracy, ładny zapach detergentów. wykonawca uprzejmy. dziękuję.",stars:3},
+    {id:1,user:"Dan",text:"jestem zadowolona ze sprzatania. punktualność, szybkość pracy, ładny zapach detergentów. wykonawca uprzejmy. dziękuję.",stars:5},
+    {id:1,user:"Dan",text:"jestem zadowolona ze sprzatania. punktualność, szybkość pracy, ładny zapach detergentów. wykonawca uprzejmy. dziękuję.",stars:4}
+
+
+
+
 ]
 
 export default function Opinions() {
