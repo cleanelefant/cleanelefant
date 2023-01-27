5d07360fb8358dbabd27bc68ac087feb572b9b64
igorraknorwid <igorrakfed@gmail.com>
2023-01-27 13:05:38 +0100

    swiper slides

--- src/components/locals/Locals.tsx
+++ src/components/locals/Locals.tsx
@@ -57,76 +57,34 @@ const slides = [
 
 const Locals = () => {
   return (
-    <Swiper
-      spaceBetween={10}
-      slidesPerView={5}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {slides.map((s) => (
-        <SwiperSlide
-          key={s.id}
-          className='bg-blue-500 text-white rounded-3xl  flex justify-center items-center'
-        >
-          <a href={s.slug}>
-            <div className='h-10 flex flex-col justify-center items-center my-40'>
-              {s.text}
-            </div>
-          </a>
-        </SwiperSlide>
-      ))}
-      {/* <SwiperSlide className='bg-blue-500 text-white rounded-3xl  flex justify-center items-center'>
-        <a href='/test'>
-          <div className='h-10 flex flex-col justify-center items-center my-40'>
-            Slide 1
-          </div>
-        </a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href='/test'>
-          {" "}
-          <div className='bg-red-500 text-white p-20 rounded-3xl'>Slide 1</div>
-        </a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href='/test'>
-          {" "}
-          <div className='bg-red-500 text-white p-20 rounded-3xl'>Slide 1</div>
-        </a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href='/test'>
-          {" "}
-          <div className='bg-red-500 text-white p-20 rounded-3xl text-center'>
-            Slide 555
-          </div>
-        </a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href='/test'>
-          {" "}
-          <div className='bg-red-500 text-white p-20 rounded-3xl'>Slide 1</div>
-        </a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href='/test'>
-          {" "}
-          <div className='bg-red-500 text-white p-20 rounded-3xl'>Slide 1</div>
-        </a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href='/test'>
-          {" "}
-          <div className='bg-red-500 text-white p-20 rounded-3xl'>Slide 1</div>
-        </a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href='/test'>
-          {" "}
-          <div className='bg-red-500 text-white p-20 rounded-3xl'>Slide 1</div>
-        </a>
-      </SwiperSlide> */}
-    </Swiper>
+    <div className='my-10'>
+      <Swiper
+        spaceBetween={5}
+        slidesPerView={5}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {slides.map((s) => (
+          <SwiperSlide
+            key={s.id}
+            className='rounded-3xl  flex justify-center items-center'
+          >
+            <a href={s.slug}>
+              <div
+                style={{
+                  backgroundImage: `url(${s.src})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "50% 50%",
+                }}
+                className='h-10 flex flex-col justify-center items-center p-28 py-48 rounded-2xl'
+              >
+                {s.text}
+              </div>
+            </a>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 export default Locals;
