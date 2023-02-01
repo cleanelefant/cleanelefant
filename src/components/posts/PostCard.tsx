7f72e89f608d4cda897de8962b226d0f6655cde9
igorraknorwid <igorrakfed@gmail.com>
2023-02-01 12:21:40 +0100

    block card height

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -10,18 +10,20 @@ interface IPostCard {
 export default function PostCard(props: IPostCard) {
   const { slug, title, description, src, double, intersection } = props;
   return (
-    <a href={"blog/"+ slug} className="flex justify-center">
-      <div className="rounded-lg shadow-lg bg-white">
+    <a href={"blog/" + slug} className='flex items-stretch min-h-full'>
+      <div className='rounded-lg shadow-lg bg-white'>
         <div>
           <img
-            className="rounded-t-lg"
+            className='rounded-t-lg'
             src={intersection ? (src ? src : double) : ""}
             alt={title}
           />
         </div>
-        <div className="py-12 px-6">
-          <h5 className="text-gray-900 text-2xl font-medium mb-4 hover:underline transition duration-1000">{title}</h5>
-          <p className="text-gray-700 text-base mb-8">{description}</p>
+        <div className='py-12 px-6 h-[380px]'>
+          <p className='text-gray-900 text-2xl font-bold mb-4 hover:underline transition duration-1000'>
+            {title}
+          </p>
+          <p className='text-gray-700 text-lg mb-8'>{description}</p>
         </div>
       </div>
     </a>
