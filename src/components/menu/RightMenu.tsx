b160c427056dca398e149b7d728693ba69cd81fb
igorraknorwid <igorrakfed@gmail.com>
2023-02-06 13:39:59 +0100

    main

--- src/components/menu/RightMenu.tsx
+++ src/components/menu/RightMenu.tsx
@@ -8,14 +8,13 @@ export default function RightMenu() {
     <motion.nav
       initial={true}
       animate={isOpen ? "open" : "closed"}
-      className="hidden lg:block fixed right-5 top-24 z-40 "
+      className='hidden lg:block fixed right-5 top-24 z-40 '
     >
       <motion.button
         whileTap={{ scale: 0.97 }}
         onClick={() => setIsOpen(!isOpen)}
-        className="w-full mx-auto flex items-center justify-center gap-x-5 my-5 bg-white rounded-xl drop-shadow-2xl"
+        className=' mx-auto flex items-center justify-center gap-x-5 my-5 p-4 bg-white rounded-full drop-shadow-2xl'
       >
-       
         <motion.div
           variants={{
             open: { rotate: 180 },
@@ -23,10 +22,9 @@ export default function RightMenu() {
           }}
           transition={{ duration: 0.2 }}
           style={{ originY: 0.55 }}
-          
         >
-          <svg width="55" height="55" fill="black" viewBox="0 0 20 20">
-            <path d="M0 7 L 20 7 L 10 16" />
+          <svg width='30' height='30' fill='black' viewBox='0 0 20 20'>
+            <path d='M0 7 L 20 7 L 10 16' />
           </svg>
         </motion.div>
       </motion.button>
@@ -53,10 +51,10 @@ export default function RightMenu() {
           },
         }}
         style={{ pointerEvents: isOpen ? "auto" : "none" }}
-        className="flex flex-col gap-y-5 p-2 drop-shadow-2xl"
+        className='flex flex-col gap-y-2 p-2 drop-shadow-2xl'
       >
         {services.map((s, index) => (
-          <motion.div          
+          <motion.div
             key={s.id}
             style={{ x: 400, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
@@ -65,17 +63,17 @@ export default function RightMenu() {
               duration: 0.5,
               delay: (1 * index) / 8,
             }}
-            className="py-1 group"
+            className='py-1 group'
           >
-            <a className="rounded-lg shadow-lg " href={s.link} key={s.id}>
+            <a className='rounded-lg shadow-lg ' href={s.link} key={s.id}>
               <img
-                className="max-w-[48px] mx-auto group-hover:scale-125 transition-transform "
+                className='max-w-[48px] mx-auto group-hover:scale-125 transition-transform '
                 src={s.src}
                 alt={s.title}
                 width={40}
                 height={40}
               />
-              <div className="text-[14px] max-w-[150px] text-center font-medium ">
+              <div className='text-[14px] max-w-[150px] text-center font-medium'>
                 {s.title}
               </div>
             </a>
