c7c7eb6361cb63bfa31c8ee3ba5203c5e572776f
igorraknorwid <igorrakfed@gmail.com>
2023-02-08 11:12:05 +0100

    content

--- src/components/order_navigator/OrderNavigator.tsx
+++ src/components/order_navigator/OrderNavigator.tsx
@@ -18,9 +18,12 @@ export default function OrderNavigator() {
 
   return (
     <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
       data-url='https://cleanelefant.netlify.app/order'
       data-base-url='https://cleanelefant.netlify.app/order'
-      className='w-11/12  lg:w-4/5 2xl:w-3/5 mx-auto flex gap-y-5 lg:gap-y-0 lg:gap-x-5 flex-col lg:flex-row lg:mt-24'
+      className='w-11/12  lg:w-4/5 2xl:w-3/5 mx-auto flex gap-y-5 lg:gap-y-0 lg:gap-x-5 flex-col lg:flex-row'
     >
       <OrderButton min={1} title={"pokój"} foo={setRoom} />
       <OrderButton min={1} title={"łazienka"} foo={setBedroom} />
