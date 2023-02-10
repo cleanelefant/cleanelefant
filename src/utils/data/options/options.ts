aa8bb300f9ff97bc56b76b55570a1f04ff90a9b6
igorraknorwid <igorrakfed@gmail.com>
2023-02-10 12:09:28 +0100

    option js

---
+++ src/utils/data/options/options.ts
@@ -0,0 +1,29 @@
+export const options = [
+  {
+    id: 1,
+    title: "Jednopokojowe",
+    price: 149.9,
+    room: 1,
+    description:
+      "Cena obejmuje sprzątanie jednego pokoju, kuchni, przedpokoju oraz jednej łazienki, raz w tygodniu",
+    itemPropName: "sprzątanie kawalerki",
+  },
+  {
+    id: 2,
+    title: "Dwupokojowe",
+    price: 179.0,
+    room: 2,
+    description:
+      "Cena obejmuje sprzątanie dwóch pokoi, kuchni, przedpokoju oraz jednej łazienki, raz w tygodniu",
+    itemPropName: "sprzątanie dwupokojowego mieszkania",
+  },
+  {
+    id: 3,
+    title: "Trzypokojowe",
+    price: 209.9,
+    room: 3,
+    description:
+      "Cena obejmuje sprzątanie trzech pokoi, kuchni, przedpokoju oraz jednej łazienki, raz w tygodniu",
+    itemPropName: "sprzątanie trzypokojowego mieszkania",
+  },
+];
