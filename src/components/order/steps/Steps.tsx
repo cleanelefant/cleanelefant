ac3efcfc5d5d32c6bcaf01e8b8c108766066e8fb
igorraknorwid <igorrakfed@gmail.com>
2023-03-16 10:20:57 +0100

    mobile order button 3

--- src/components/order/steps/Steps.tsx
+++ src/components/order/steps/Steps.tsx
@@ -6,13 +6,13 @@ function ChoosePayment() {
   const { store } = useContext(Context);
 
   return (
-    <div className='sticky top-0 z-50 flex flex-col lg:flex-row bg-lime-200 font-mono'>
+    <div className='sticky top-0 z-50 flex flex-col lg:flex-row bg-cyan-400 font-mono'>
       {store.steps?.map((step, i) => (
         <a
           key={step.id}
           className={`${
-            step.isActive && "text-white bg-lime-600"
-          } basis-1/4 text-center hover:bg-lime-300 p-px lg:py-4 cursor-pointer border tracking-wider`}
+            step.isActive && "text-white bg-cyan-700"
+          } basis-1/4 text-center hover:bg-cyan-500 p-px lg:py-4 cursor-pointer border tracking-wider`}
           href={step.target}
         >
           <p>
