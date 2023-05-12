6b38e38ca5d01746b098786d949232a64ef5bbb2
igorraknorwid <igorrakfed@gmail.com>
2023-05-12 12:18:11 +0200

    renovation datepicker css

--- src/components/renovation/additional_services/MultyWashingCard.tsx
+++ src/components/renovation/additional_services/MultyWashingCard.tsx
@@ -76,18 +76,18 @@ function MultyWashingCard({ item }: IMultyCard) {
       <p className='text-center'>{item.title}</p>
       <p className='lg:text-xl'>{item.price} zł.</p>
       {item.isActive && (
-        <div className='flex'>
+        <div className='flex lg:gap-x-4'>
           <button
-            className='my-button cursor-pointer p-2 border'
+            className='my-button cursor-pointer py-2 px-4 border-4 bg-blue-600 hover:bg-blue-400'
             onClick={() => {
               buttonDecreaseClickHandler(item);
             }}
           >
             -
           </button>
-          <div className='py-2 px-4'>{total}</div>
+          <div className='py-2 px-4 lg:text-xl'>{total}</div>
           <button
-            className='my-button cursor-pointer p-2 border'
+            className='my-button cursor-pointer py-2 px-4 border-4 bg-blue-600 hover:bg-blue-300 '
             onClick={() => {
               buttonIncreaseClickHandler(item);
             }}
